053971b05369f2e51a6625c5b3d773a0901e065f
ninadkadam123 <ninadkadam123@gmail.com>
2023-01-25 23:50:02 +0530

    prog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListComponent } from './components/list/list.component';
+import { AddComponent } from './components/add/add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    ListComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
