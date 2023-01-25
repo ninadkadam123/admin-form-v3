f17c63e9738fe2607cdc5e0ace4240ced12429f3
ninadkadam123 <ninadkadam123@gmail.com>
2023-01-25 23:48:20 +0530

    commit2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './components/add/add.component';
+import { ListComponent } from './components/list/list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'',component:ListComponent},
+  {path:'/',component:ListComponent},
+  {path:'/add',component:AddComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
